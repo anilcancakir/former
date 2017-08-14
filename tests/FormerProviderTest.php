7893a05b3015355b6e7a6bd8f048e87c3b047e44
Anılcan ÇAKIR <anilcan@kodizm.com>
2017-08-15 01:41:06 +0300

    Initial commit.

--- tests/FormerProviderTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests;
-
-use PHPUnit_Framework_TestCase;
-
-class FormerProviderTest extends PHPUnit_Framework_TestCase {
-
-}
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AnilcanCakir\Former\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    /** @test */
+    public function testIsWorking() {
+        $this->assertTrue(true);
+    }
+}
